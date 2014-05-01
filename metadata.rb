491d57d92be62b5c73349e03bffba1da71ffd290
Roberto Garza <roberto.garza@rackspace.com>
2014-05-01 10:55:29 -0500

    fixed type in recipe/default.rb

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures memcache_wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
-depends			 "rackspace_memcached" 
-depends			 "rackspace_iptables"
+depends			 "rackspace_memcached", "> 0.1.0"
+depends			 "rackspace_iptables", "> 1.0"
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "racksapce_memcached"
+include_recipe "rackspace_memcached"
 include_recipe "rackspace_iptables"
 
 add_iptables_rule('INPUT', '-p tcp --dport 11211 -j ACCEPT', 'allow memcached')
